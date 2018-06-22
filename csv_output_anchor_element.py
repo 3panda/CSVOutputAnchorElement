837172fe87b9efd6b5824f8af40e5ae046ade941
3panda <3panda@gmail.com>
2018-06-22 11:08:49 +0900

    CSVの出力処理

--- csv_output_anchor_element.py
+++ csv_output_anchor_element.py
@@ -2,19 +2,44 @@
 # -*- coding: utf-8 -*-
 import sys
 import requests
+import csv
 
 
-def main(target_url):
+def main(target_url: str):
     print(target_url)
     url = target_url
     try:
         r = requests.get(url)
-        print(r.text)
+        with open('get.html', mode='w') as f:
+            f.write(r.text)
+            print(r.text)
+
+        # CSVに書き出し
+        csv_output()
+
     except requests.exceptions.RequestException as err:
         print(target_url + "の取得に失敗しました")
         print("エラーの内容は以下の通りです")
         print("---------------------")
         print(err)
+
+
+def csv_output(output_name: str ="anchor_element.csv"):
+    print(output_name + "に書き出し")
+
+    body = [
+        ['タイトル1', 'title1', 'title1.html'],
+        ['タイトル2', 'title2', 'title2.html'],
+        ['タイトル3', '', 'title3.html'],
+        ['タイトル4', 'title4', 'title4.html'],
+        ['タイトル5', '', 'title5.html'],
+    ]
+    with open(output_name, 'w') as f:
+        # writerオブジェクト作成
+        writer = csv.writer(f)
+        # 内容を書き込む
+        for b in body:
+            writer.writerow(b)
 
 
 if __name__ == '__main__':
