36ac5cca4281d267222d91c96fa2b6bd50e636a4
3panda <3panda@gmail.com>
2018-06-25 00:14:23 +0900

    htmlを解析しCSVに書き出す処理作成

--- csv_output_anchor_element.py
+++ csv_output_anchor_element.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import requests
+import bs4
 import csv
 from typing import List
 
@@ -9,38 +10,28 @@ from typing import List
 def main(target_url: str):
     print(target_url)
     url = target_url
+    a_elem_data = []
     try:
         r = requests.get(url)
         with open('get.html', mode='w') as f:
             f.write(r.text)
             print(r.text)
 
-        get_a_data = [
-                ['タイトル2', 'title2', 'title2.html'],
-                ['タイトル5', '', 'title2.html'],
-                ['タイトル3', '', 'title3.html'],
-                ['タイトル4', 'title4', 'title4.html'],
-                ['タイトル2', '', 'title2.html'],
-                ['タイトル2', 'title2', 'title2.html'],
-                ['タイトル3', '', '3.html'],
-                ['タイトル2', 'title2', 'title2.html'],
-                ['タイトル3', '', 'title3.html'],
-                ['タイトル4', '4', 'title4.html'],
-                ['タイトル2', 'title2', 'title2.html'],
-                ['タイトル3', '', 'title3.html'],
-                ['タイトル4', 'title4', 'title4.html'],
-                ['タイトル4', '4_', 'title.html'],
-                ['タイトル3', '', 'title3.html'],
-                ['タイトル4', 'title4', 'title4.html'],
-                ['タイトル1', 'title1', 'title1.html'],
-                ['タイトル5', '', 'title2.html'],
-                ['タイトル5', '', 'title4.html'],
-            ]
-
-        # 前処理でソート Pythonの場合Listのソートは内部ソートが高速なので使用
-        get_a_data.sort()
+            soup = bs4.BeautifulSoup(r.text, "html.parser")
+            elems = soup.select('a')
+            for elem in elems:
+                get_elem = []
+                get_elem.append(elem.getText().replace('\n', ''))
+                title = elem.get('title')
+                if (title is None):
+                    title = ''
+                get_elem.append(title.replace('\n', ''))
+                get_elem.append(elem.get('href').replace('\n', ''))
+                a_elem_data.append(get_elem)
+        # ソートしておく
+        a_elem_data.sort()
         # 重複データの削除
-        formatted = deleted_to_duplicate_data(get_a_data)
+        formatted = deleted_to_duplicate_data(a_elem_data)
 
         # CSVに書き出し
         csv_output(formatted)
@@ -56,8 +47,6 @@ def deleted_to_duplicate_data(l: List[List[str]]) -> List[List[str]]:
     check_list = l
     del_num = []
     for i in range(len(l)):
-        print("i:" + str(i))
-        print(del_num)
         # 削除対象は探索しない
         if (i in del_num):
             break
@@ -68,11 +57,12 @@ def deleted_to_duplicate_data(l: List[List[str]]) -> List[List[str]]:
                 duplicate_value = list(set(l[i]) & set(check_list[j]))
                 duplicate_number = len(duplicate_value)
                 # 重複要素　全ての場合は即削除
+                # TODO: リンクテキスト, title属性, href属性のつ
                 if duplicate_number == 3:
                     check_list[j] = ''
                 # 重複要素 二つの場合は空が無ければ削除
                 elif duplicate_number == 2:
-                    if ("" not in duplicate_value):
+                    if ('' not in duplicate_value):
                         check_list[j] = ''
     # 空白を削除して返す
     return filter(lambda str: str != '', check_list)
@@ -80,9 +70,10 @@ def deleted_to_duplicate_data(l: List[List[str]]) -> List[List[str]]:
 
 def csv_output(body: List[List[str]], output_name: str = "anchor_element.csv"):
     print(output_name + "に書き出し")
-    with open(output_name, 'w') as f:
+    with open(output_name, 'w', newline='') as f:
         # writerオブジェクト作成
-        writer = csv.writer(f)
+        writer = csv.writer(
+                 f, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
         # 内容を書き込む
         for b in body:
             writer.writerow(b)
